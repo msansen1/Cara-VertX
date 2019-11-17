fff3bae7d4012d9815cd901db4d3961073ebb522
mathieu <msansen@gmail.com>
2019-11-17 16:55:20 +0100

    WIP: ajout du Menu

--- src/main/java/com/cara/vertx/Verticles/RestaurantVerticle.java
+++ src/main/java/com/cara/vertx/Verticles/RestaurantVerticle.java
@@ -6,10 +6,21 @@ import io.vertx.core.shareddata.AsyncMap;
 import io.vertx.core.shareddata.Counter;
 import io.vertx.core.shareddata.SharedData;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RestaurantVerticle extends AbstractVerticle {
+
+
+  //La carte du restaurant
+  public static ArrayList<String> menu = (ArrayList<String>) Stream.of("La carbonade flamande", "Welsh", "Le chicon-gratin", "Joues de porc au maroilles", "Flamiche au maroilles", "Plat du Jour", "Tajine", "Couscous Royal").collect(Collectors.toList());
+
+
   @Override
   public void start() throws Exception {
     System.out.println("Start of Restaurant Verticle");
+
 
     //On définit la capacité du restaurant dans un compteur
     vertx.sharedData().getCounter(
