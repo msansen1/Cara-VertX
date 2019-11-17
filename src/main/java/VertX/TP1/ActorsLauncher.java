134cec5336635cf76cb76f776a7108a278f1ead9
AnassYK <anassyakine66@gmail.com>
2019-11-17 02:46:44 +0100

    ajout des handlers

--- src/main/java/VertX/TP1/ActorsLauncher.java
+++ src/main/java/VertX/TP1/ActorsLauncher.java
@@ -4,7 +4,9 @@ import VertX.TP1.Actors.ChefVerticle;
 import VertX.TP1.Actors.CuisinierVerticle;
 import VertX.TP1.Actors.RestaurantVerticle;
 import VertX.TP1.Actors.ServeurVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 
 public class ActorsLauncher {
@@ -13,21 +15,23 @@ public class ActorsLauncher {
     System.out.println("Start of Actors Launcher");
 
     final Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(RestaurantVerticle.class.getName(), ar -> {
+    final DeploymentOptions serveurOptions = new DeploymentOptions().setInstances(3);
+    final DeploymentOptions cuisinierOptions = new DeploymentOptions().setInstances(2);
+
+    final Handler<AsyncResult<String>> restaurantCompletionHandler = sar -> {
       System.out.println("Restaurant Verticle Deployed");
 
       //Chef
       vertx.deployVerticle(ChefVerticle.class.getName());
 
       //Serveur
-      final DeploymentOptions serveurOptions = new DeploymentOptions().setInstances(3);
       vertx.deployVerticle(ServeurVerticle.class.getName(),serveurOptions);
 
       //Cuisinier
-      final DeploymentOptions cuisinierOptions = new DeploymentOptions().setInstances(2);
       vertx.deployVerticle(CuisinierVerticle.class.getName(),cuisinierOptions);
+    };
 
-    });
+    vertx.deployVerticle(RestaurantVerticle.class.getName(),restaurantCompletionHandler);
 
     System.out.println("End of Actors Launcher");
 
